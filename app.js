3ead396125abb5ccea77c43768533db8fa49c159
OMG Kotofey <omgkotofey@gmail.com>
2022-03-26 22:01:41 +0000

    Created url management handling feature

---
+++ app.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const dns = require('dns');
+const url = require('url');
+const randomstring = require('randomstring');
+
+mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true });
+
+const ShortUrl = mongoose.model('ShortUrl', new mongoose.Schema({
+  original_url: String,
+  short_url: String,
+}));
+
+const UrlValidator = {
+  validate: (givenUrl) => dns.promises.lookup(
+    (new URL(givenUrl)).host,
+    {
+      hints: dns.ADDRCONFIG | dns.V4MAPPED,
+    },
+  ).then((result) => givenUrl)
+}
+
+const ShortUrlManager = {
+  createUrl: (original_url, short_code = null) =>
+    UrlValidator
+      .validate(original_url)
+      .then((original_url) => ShortUrl.create({
+        original_url: original_url,
+        short_url: short_code || randomstring.generate(7)
+      }))
+  ,
+
+  findUrl: (short_url, callback) => {
+    ShortUrl.findOne(
+      { "short_url": short_url },
+      callback
+    );
+  }
+}
+
+exports.ShortUrlModel = ShortUrl;
+exports.ShortUrlManager = ShortUrlManager;
--- server.js
+++ server.js
@@ -1,22 +1,52 @@
-require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const url_manager = require('./app.js').ShortUrlManager;
+
 const app = express();
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env['PORT'] || 3000;
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', (req, res) => {
+  return res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/hello', (req, res) => {
+  return res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl', urlencodedParser, (req, res) => {
+  original_url = req.body.url;
+
+  if (!original_url) {
+    return res
+      .status(400)
+      .json({
+        error: 'no url given'
+      });
+  }
+
+  url_manager
+  .createUrl(original_url)
+  .then(
+    (url) => res.json({
+      original_url: url.original_url,
+      short_url: url.short_url
+    })
+  ).catch((err) => 
+    res
+    .status(400)
+    .json({
+      error: 'invalid url'
+    }));
 });
 
 app.listen(port, function() {
